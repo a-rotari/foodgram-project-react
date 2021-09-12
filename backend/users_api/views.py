a80a183da000d69efed988883bd26cdae3b4cf2a
Andrei Rotari <andreichina@gmail.com>
2021-09-12 22:08:38 +0300

    Added view to create and list users.

--- backend/users_api/views.py
+++ backend/users_api/views.py
@@ -1,11 +1,23 @@
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics, permissions
 
 from .models import User
-from .serializers import UserSerializer
+from . import serializers, paginate
 
 
-class UserListCreate(ListCreateAPIView):
+
+class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    pagination_class = PageNumberPagination
+    serializer_class = serializers.UserSerializer
+    pagination_class = paginate.UserPagination
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        print(self.request.method)
+        if self.request.method == 'GET':
+            self.permission_classes = [permissions.IsAuthenticated]
+        else:
+            self.permission_classes = [permissions.AllowAny]
+        print(self.permission_classes)
+        return [permission() for permission in self.permission_classes]
