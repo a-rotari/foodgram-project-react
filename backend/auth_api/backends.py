bf29bb9250d4cb5f7aea085beb46a0ed0a106420
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-10-17 18:15:25 +0300

    Added docstring to model and checked with flake8.

--- backend/auth_api/backends.py
+++ backend/auth_api/backends.py
@@ -5,15 +5,20 @@ from users_api.models import User
 
 
 class CustomModelBackend(ModelBackend):
+    """ Custom authentication backend model that authenticates using
+        email/password instead of username/password. """
+
     def authenticate(self, request, email=None, password=None, **kwargs):
         if email is None:
             email = kwargs.get(User.email)
         if email is None or password is None:
-            return
+            return None
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
+            # run the hasher (see explanation in the parent model)
             User().set_password(password)
         else:
-            if user.check_password(password) and self.user_can_authenticate(user):
+            if (user.check_password(password)
+                    and self.user_can_authenticate(user)):
                 return user
