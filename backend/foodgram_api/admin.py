aa8a21f1ea7eaa6c0d8bc875f8a606f426830c94
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-10-17 19:28:30 +0300

    Flake8, added dosctrings to admin models.

--- backend/foodgram_api/admin.py
+++ backend/foodgram_api/admin.py
@@ -4,14 +4,21 @@ from .models import Favorite, Ingredient, Portion, Recipe, ShoppingCart, Tag
 
 
 class PortionInLine(admin.TabularInline):
+    """ Admin model for displaying portions (ingredients + amounts). """
+
     model = Portion
     extra = 1
 
+
 class RecipeAdmin(admin.ModelAdmin):
+    """ Admin model for Recipe objects. """
     inlines = (PortionInLine,)
 
+
 class IngredientAdmin(admin.ModelAdmin):
+    """ Admin model for Ingredient objects. """
     inlines = (PortionInLine,)
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
