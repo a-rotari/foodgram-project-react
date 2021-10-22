f36d4a9476b4c0a120e7cbacc4cdbfa184ec53e7
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-10-22 20:28:39 +0300

    Run run run

--- backend/load_ingredients.py
+++ backend/load_ingredients.py
@@ -1,14 +1,19 @@
 import json
 from foodgram_api.models import Ingredient, Tag
 
-with open('ingredients.json', encoding='utf-8') as data_file:
-    json_data = json.loads(data_file.read())
+i = Ingredient.objects.all()
+t = Tag.objects.all()
 
-    for ingredient_data in json_data:
-        Ingredient.objects.create(**ingredient_data)
+if not i:
+    with open('ingredients.json', encoding='utf-8') as data_file:
+        json_data = json.loads(data_file.read())
 
-with open('tags.json', encoding='utf-8') as data_file:
-    json_data = json.loads(data_file.read())
+        for ingredient_data in json_data:
+            Ingredient.objects.create(**ingredient_data)
 
-    for tag_data in json_data:
-        Tag.objects.create(**tag_data)
+if not t:
+    with open('tags.json', encoding='utf-8') as data_file:
+        json_data = json.loads(data_file.read())
+
+        for tag_data in json_data:
+            Tag.objects.create(**tag_data)
