789346b661135b0d3ff3b6e9f24c067e9444b497
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-10-23 23:09:17 +0300

    Fix 63

--- backend/foodgram_api/serializers.py
+++ backend/foodgram_api/serializers.py
@@ -1,26 +1,12 @@
-import base64
 import json
-import uuid
 from unittest.mock import Base
 
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 from users_api.serializers import UserSerializerFull
 
+from .fields import Base64ImageField
 from .models import Favorite, Ingredient, Portion, Recipe, Tag
-
-
-class Base64ImageField(serializers.FileField):
-    """ This is a custom field that handles Base64 image data. """
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            id = uuid.uuid4()
-            data = ContentFile(base64.b64decode(imgstr),
-                               name=id.urn[9:] + '.' + ext)
-        return super(Base64ImageField, self).to_internal_value(data)
 
 
 class TagSerializer(serializers.ModelSerializer):
